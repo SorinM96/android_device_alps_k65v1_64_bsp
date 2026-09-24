d01146af5619e74a9936db38ab3776fd87031330
SorinM96 <meressorin@gmail.com>
2026-09-25 07:39:23 +0800

    Update omni_k65v1_64_bsp.mk

--- omni_k65v1_64_bsp.mk
+++ omni_k65v1_64_bsp.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from k65v1_64_bsp device
 $(call inherit-product, device/alps/k65v1_64_bsp/device.mk)
